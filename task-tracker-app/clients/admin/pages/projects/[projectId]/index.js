9655cb33e3e3246e91e97d30d99211d9a2f6a40a
AbhishekPuranam <apuranam@netapp.com>
2025-11-06 16:59:46 +0530

    Add Navbar to project detail page for consistent UX theme

--- task-tracker-app/clients/admin/pages/projects/[projectId]/index.js
+++ task-tracker-app/clients/admin/pages/projects/[projectId]/index.js
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import ProtectedRoute from '../../../components/ProtectedRoute';
+import Navbar from '../../../components/Navbar';
 import SubProjectManagement from '../../../components/SubProjects/SubProjectManagement';
 
 export default function ProjectSubProjectsPage() {
   return (
     <ProtectedRoute allowedRoles={['admin']}>
+      <Navbar />
       <SubProjectManagement />
     </ProtectedRoute>
   );
